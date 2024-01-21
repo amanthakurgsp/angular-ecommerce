c68933d22aaf24c5c260019de01613fd23c1ab31
amanthakurgsp <amanthakur8625@gmail.com>
2024-01-21 12:32:55 +0530

    ecommerce

--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BuyerDashboardComponent } from './buyer/buyer-dashboard/buyer-dashboard.component';
+import { CheckoutComponent } from './buyer/checkout/checkout.component';
+import { SellerDashboardComponent } from './seller/seller-dashboard/seller-dashboard.component';
+import { SigninSignupComponent } from './signin-signup/signin-signup.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BuyerDashboardComponent,
+    CheckoutComponent,
+    SellerDashboardComponent,
+    SigninSignupComponent
+  ],
   imports: [
     CommonModule
   ]
---
+++ src/app/customer/signin-signup/signin-signup.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signin-signup',
+  templateUrl: './signin-signup.component.html',
+  styleUrls: ['./signin-signup.component.scss']
+})
+export class SigninSignupComponent {
+
+}
