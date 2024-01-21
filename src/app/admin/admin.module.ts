bcd2f65ca60411a8344292dc06ff8baa7b78f64a
amanthakurgsp <amanthakur8625@gmail.com>
2024-01-21 12:25:17 +0530

    ecommerce

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { UserCrudComponent } from './user-crud/user-crud.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AdminDashboardComponent,
+    AdminLoginComponent,
+    UserCrudComponent
+  ],
   imports: [
     CommonModule
   ]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { ProductComponent } from './product/product.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactUsComponent,
+    ProductComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
