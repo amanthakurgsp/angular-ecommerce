8d0bad7b3f86999b25cc9a0e77e27fbb46edc736
amanthakurgsp <amanthakur8625@gmail.com>
2024-01-21 12:38:33 +0530

    ecommerce

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NumberOnlyDirective } from './directives/number-only.directive';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { PageNotFoundComponent } from './layouts/page-not-found/page-not-found.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NumberOnlyDirective,
+    HeaderComponent,
+    FooterComponent,
+    PageNotFoundComponent
+  ],
   imports: [
     CommonModule
   ]
